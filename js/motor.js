a8da71119fa67a355f0f102de5f9cc6b3bb471ac
Borjaarmet <borjaarmet@gmail.com>
2021-12-03 12:40:47 +0100

    add images, sounds and js functionality

--- js/motor.js
+++ js/motor.js
@@ -1,1 +1,28 @@
-console.log("hello");
+let estadoLicuadora = "apagada";
+let licuadora = document.getElementById("blender");
+let sonidoLicuadora = document.getElementById("blender-sound");
+let sonidoBtnLicuadora = document.getElementById("blender-btn-sound");
+
+function controlarLicuadora() {
+  if (estadoLicuadora == "apagada") {
+    estadoLicuadora = "encendido";
+    licuadora.classList.add("active");
+    playSonido();
+    console.log("encendida!!");
+  } else {
+    estadoLicuadora = "apagada";
+    licuadora.classList.remove("active");
+    playSonido();
+    console.log("apagada");
+  }
+}
+function playSonido() {
+  if (sonidoLicuadora.paused) {
+    sonidoBtnLicuadora.play();
+    sonidoLicuadora.play();
+  } else {
+    sonidoBtnLicuadora.play();
+    sonidoLicuadora.pause();
+    sonidoLicuadora.currentTime = 0;
+  }
+}
